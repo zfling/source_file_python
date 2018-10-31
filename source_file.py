426c6ff9fa80229c1350a478692cf2ecffc1136d
zfling <18609265862@163.com>
2018-10-31 22:48:19 +0800

    Author:zfl 【python3】【功能：根据uboot linux编译过程文件输出源文件、头文件列表用于source insight工程】

---
+++ source_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#######################################
+# 解析uboot linux包含的源文件、头文件
+# 用于Source Insight工程
+#######################################
+#fread = open('uboot_vexpress_log.txt')
+
+def myfunction():
+    print("函数调用")
+
+if __name__=='__main__':
+    print("test")
+    myfunction()
